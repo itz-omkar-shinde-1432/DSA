1c7309fa80504c76a56b7d9b56a3f019a9e4c673
Omkar Shinde <omkar.shinde.work.dev@gmail.com>
2025-08-02 14:29:23 +0530

    modified:   Insertion_Sort.java

--- Sorting/Insertion_Sort.java
+++ Sorting/Insertion_Sort.java
@@ -1,5 +1,44 @@
 import java.util.Scanner;
 public class Insertion_Sort {
-    
+
+    public static void InsertionSort(int arr[],int n){
+
+        for(int i=0;i<=n-1;i++){
+                int j=i;
+                while(j>0 && arr[j-1]>arr[j]){
+
+                    int temp = arr[j];
+                    arr[j]= arr[j-1];
+                    arr[j-1]= temp;
+                
+
+
+                    j--;
+
+                }
+        }
+    }
+
+
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of elements:");
+        int n= input.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.print("Enter the elements of the array:");
+        for(int i=0;i<n;i++){
+            arr[i] = input.nextInt();
+        }
+
+        InsertionSort(arr, n);
+
+        System.out.print("Sorted Array:");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
     
 }
